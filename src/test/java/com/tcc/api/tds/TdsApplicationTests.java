ab41a81400e936bd5dbad6df981fd082b36cf5d6
thisLucio <luxusnietzsche@gmail.com>
2021-09-04 01:15:20 -0300

    Commit inicial do projeto

---
+++ src/test/java/com/tcc/api/tds/TdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.api.tds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
